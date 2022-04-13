f5cd2bb61442f413fd596414a8bf44245f830f33
StarkovaYulia <starconstyulia@yandex.ru>
2022-04-13 23:57:21 +0700

    E2E tests

--- E2ETests.py
+++ E2ETests.py
@@ -14,6 +14,17 @@ def test_empty_string(app):
     app.pushButton.click()
     assert "" == app.label_2.toPlainText()
 
+def test_correct_time(app):
+    app.textEdit.setText("06:00")
+    app.pushButton.click()
+    assert "06:00" == app.label_2.toPlainText()
+
+def test_correct_time_with_spaces(app):
+    app.textEdit.setText(" 06:00  ")
+    app.pushButton.click()
+    assert "06:00" == app.label_2.toPlainText()
+
+
 
 
 
